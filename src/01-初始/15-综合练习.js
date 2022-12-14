efae2862a79a0e0062b9a15618adf4291c0a037f
wangshuhua <1730129114@qq.com>
2022-12-14 10:32:48 +0800

    feat: optimize

--- src/01-初始/15-综合练习.js
+++ src/01-初始/15-综合练习.js
@@ -4,7 +4,7 @@
  * @Author: wangshuhua
  * @Date: 2022-12-07 09:55:06
  * @LastEditors: wangshuhua
- * @LastEditTime: 2022-12-07 15:26:07
+ * @LastEditTime: 2022-12-14 10:31:27
  */
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
@@ -27,183 +27,205 @@ const h = window.innerHeight;
 const groundW = 50;
 const groundH = 10;
 
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(45, w / h, 0.1, 100);
-camera.position.set(0, 0.2, 5);
-camera.lookAt(0, 0, 0);
 
-// light
-const ambientLight = new THREE.AmbientLight(0xffffff, 0.2);
-scene.add(ambientLight);
-const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
-directionalLight.position.set(0, 1, 1);
-scene.add(directionalLight);
+let scene, camera, ambientLight, renderer, controls, clock;
+let cloudGroup, cloudGroup1, treeGroup
+start();
 
-// 地面
-const groundGroup = new THREE.Group();
+function start() {
+    initScene();
+    initCamera();
+    initLight();
+    initObject();
+    initThree();
+    animate();
+}
+function initThree() {
+    renderer = new THREE.WebGLRenderer();
+    renderer.setSize(w, h);
+    renderer.setClearColor(colors.background);
+    document.body.appendChild(renderer.domElement);
+    controls = new OrbitControls(camera, renderer.domElement);
+    clock = new THREE.Clock();
+}
+function initScene() {
+    scene = new THREE.Scene();
+}
 
-const roadGroup = new THREE.Group();
+function initLight() {
+    // light
+    ambientLight = new THREE.AmbientLight(0xffffff, 0.2);
+    scene.add(ambientLight);
+    const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
+    directionalLight.position.set(0, 1, 1);
+    scene.add(directionalLight);
+}
 
-const planeGeometry = new THREE.PlaneGeometry(2, groundH);
-const planeMaterial = new THREE.MeshStandardMaterial({
-    color: colors.road,
-});
-const plane = new THREE.Mesh(planeGeometry, planeMaterial);
+function initCamera() {
+    camera = new THREE.PerspectiveCamera(45, w / h, 0.1, 100);
+    camera.position.set(0, 0.2, 5);
+    camera.lookAt(0, 0, 0);
+}
 
-const leftLineMaterial = new THREE.MeshStandardMaterial({
-    color: colors.line,
-});
-const leftLine = new THREE.Mesh(
-    new THREE.PlaneGeometry(0.1, groundH),
-    leftLineMaterial
-);
-leftLine.position.z = 0.001;
-leftLine.position.x = -0.6;
+function initObject() {
 
-const rightLine = leftLine.clone();
-rightLine.position.z = 0.001;
-rightLine.position.x = 0.6;
+    // 地面
+    const groundGroup = new THREE.Group();
 
-const dashGroup = new THREE.Group();
+    const roadGroup = new THREE.Group();
 
-let total = 21;
-for (let i = 0; i < total; i++) {
-    const dash = new THREE.Mesh(
-        new THREE.PlaneGeometry(0.1, 0.3),
-        new THREE.MeshStandardMaterial({ color: colors.line }),
+    const planeGeometry = new THREE.PlaneGeometry(2, groundH);
+    const planeMaterial = new THREE.MeshStandardMaterial({
+        color: colors.road,
+    });
+    const plane = new THREE.Mesh(planeGeometry, planeMaterial);
+
+    const leftLineMaterial = new THREE.MeshStandardMaterial({
+        color: colors.line,
+    });
+    const leftLine = new THREE.Mesh(
+        new THREE.PlaneGeometry(0.1, groundH),
+        leftLineMaterial
+    );
+    leftLine.position.z = 0.001;
+    leftLine.position.x = -0.6;
+
+    const rightLine = leftLine.clone();
+    rightLine.position.z = 0.001;
+    rightLine.position.x = 0.6;
+
+    const dashGroup = new THREE.Group();
+
+    let total = 21;
+    for (let i = 0; i < total; i++) {
+        const dash = new THREE.Mesh(
+            new THREE.PlaneGeometry(0.1, 0.3),
+            new THREE.MeshStandardMaterial({ color: colors.line }),
+        )
+        dash.position.z = 0.001;
+        dash.position.y = - 0.5 * groundH + 0.5 * i;
+        dashGroup.add(dash);
+    }
+    // 树木
+
+    treeGroup = new THREE.Group();
+    const leftTreeGroup = new THREE.Group();
+    const singleTreeGroup = new THREE.Group();
+
+    const topTree = new THREE.Mesh(
+        new THREE.ConeGeometry(0.2, 0.3, 5),
+        new THREE.MeshStandardMaterial({ color: colors.tree }),
     )
-    dash.position.z = 0.001;
-    dash.position.y = - 0.5 * groundH + 0.5 * i;
-    dashGroup.add(dash);
-}
-// 树木
 
-const treeGroup = new THREE.Group();
-const leftTreeGroup = new THREE.Group();
-const singleTreeGroup = new THREE.Group();
+    const middleTree = new THREE.Mesh(
+        new THREE.ConeGeometry(0.3, 0.4, 5),
+        new THREE.MeshStandardMaterial({ color: colors.tree }),
+    )
+    const bottomTree = new THREE.Mesh(
+        new THREE.CylinderGeometry(0.05, 0.05, 0.2),
+        new THREE.MeshStandardMaterial({ color: colors.post }),
+    )
+    const treeShadow = new THREE.Mesh(
+        new THREE.CircleGeometry(0.3, 5),
+        new THREE.MeshStandardMaterial({ color: colors.shadow }),
+    );
+    bottomTree.position.y = 0.1;
+    middleTree.position.y = 0.4;
+    topTree.position.y = 0.6;
 
-const topTree = new THREE.Mesh(
-    new THREE.ConeGeometry(0.2, 0.3, 5),
-    new THREE.MeshStandardMaterial({ color: colors.tree }),
-)
+    treeShadow.position.y = 0.001;
+    treeShadow.rotation.x = -0.5 * Math.PI
+    singleTreeGroup.add(topTree, middleTree, bottomTree, treeShadow);
+    singleTreeGroup.scale.set(0.6, 0.6, 0.6)
 
-const middleTree = new THREE.Mesh(
-    new THREE.ConeGeometry(0.3, 0.4, 5),
-    new THREE.MeshStandardMaterial({ color: colors.tree }),
-)
-const bottomTree = new THREE.Mesh(
-    new THREE.CylinderGeometry(0.05, 0.05, 0.2),
-    new THREE.MeshStandardMaterial({ color: colors.post }),
-)
-const treeShadow = new THREE.Mesh(
-    new THREE.CircleGeometry(0.3, 5),
-    new THREE.MeshStandardMaterial({ color: colors.shadow }),
-);
-bottomTree.position.y = 0.1;
-middleTree.position.y = 0.4;
-topTree.position.y = 0.6;
+    const treeNom = 11;
+    for (let i = 0; i < treeNom; i++) {
+        const tree = singleTreeGroup.clone();
+        tree.position.z = -0.5 * groundH + i * 1;
+        tree.position.x = 0.001;
+        leftTreeGroup.add(tree);
+    }
+    leftTreeGroup.position.x = - 1.2;
 
-treeShadow.position.y = 0.001;
-treeShadow.rotation.x = -0.5 * Math.PI
-singleTreeGroup.add(topTree, middleTree, bottomTree, treeShadow);
-singleTreeGroup.scale.set(0.6, 0.6, 0.6)
+    const rightTreeGroup = leftTreeGroup.clone();
+    rightTreeGroup.position.x = 1.2;
+    treeGroup.add(leftTreeGroup, rightTreeGroup);
 
-const treeNom = 11;
-for (let i = 0; i < treeNom; i++) {
-    const tree = singleTreeGroup.clone();
-    tree.position.z = -0.5 * groundH + i * 1;
-    tree.position.x = 0.001;
-    leftTreeGroup.add(tree);
-}
-leftTreeGroup.position.x = - 1.2;
-
-const rightTreeGroup = leftTreeGroup.clone();
-rightTreeGroup.position.x = 1.2;
-treeGroup.add(leftTreeGroup, rightTreeGroup);
-
-// 草地
-const frontGrass = new THREE.Mesh(
-    new THREE.PlaneGeometry(groundW, groundH / 2),
-    new THREE.MeshStandardMaterial({
-        color: colors.frontGrass,
-    })
-);
-frontGrass.position.z = -0.001;
-frontGrass.position.y = groundH / 4;
-
-const backGrass = new THREE.Mesh(
-    new THREE.PlaneGeometry(groundW, groundH / 2),
-    new THREE.MeshStandardMaterial({
-        color: colors.backGrass,
-    })
-);
-backGrass.position.z = -0.001;
-backGrass.position.y = - groundH / 4;
-// 楼宇
-const buildGroup = new THREE.Group();
-const buildTotal = 20;
-for (let i = 0; i < buildTotal; i++) {
-    const width = Math.random() + 0.5;
-    const height = Math.random() + 1;
-    const depth = Math.random();
-    const build = new THREE.Mesh(
-        new THREE.BoxGeometry(width, height, depth),
+    // 草地
+    const frontGrass = new THREE.Mesh(
+        new THREE.PlaneGeometry(groundW, groundH / 2),
         new THREE.MeshStandardMaterial({
-            color: colors.build,
+            color: colors.frontGrass,
         })
     );
-    build.position.z = - groundH / 2;
-    build.position.y = height / 2;
-    build.position.x = - groundW / 2 + 2 * i + (Math.random() - 0.5) * 5;
-    buildGroup.add(build);
+    frontGrass.position.z = -0.001;
+    frontGrass.position.y = groundH / 4;
+
+    const backGrass = new THREE.Mesh(
+        new THREE.PlaneGeometry(groundW, groundH / 2),
+        new THREE.MeshStandardMaterial({
+            color: colors.backGrass,
+        })
+    );
+    backGrass.position.z = -0.001;
+    backGrass.position.y = - groundH / 4;
+    // 楼宇
+    const buildGroup = new THREE.Group();
+    const buildTotal = 20;
+    for (let i = 0; i < buildTotal; i++) {
+        const width = Math.random() + 0.5;
+        const height = Math.random() + 1;
+        const depth = Math.random();
+        const build = new THREE.Mesh(
+            new THREE.BoxGeometry(width, height, depth),
+            new THREE.MeshStandardMaterial({
+                color: colors.build,
+            })
+        );
+        build.position.z = - groundH / 2;
+        build.position.y = height / 2;
+        build.position.x = - groundW / 2 + 2 * i + (Math.random() - 0.5) * 5;
+        buildGroup.add(build);
+    }
+
+    // 白云
+    cloudGroup = new THREE.Group();
+    const cloud1 = new THREE.Mesh(
+        new THREE.SphereGeometry(0.4),
+        new THREE.MeshBasicMaterial({ color: colors.cloud }),
+    )
+    const cloud2 = new THREE.Mesh(
+        new THREE.SphereGeometry(0.6),
+        new THREE.MeshBasicMaterial({ color: colors.cloud }),
+    )
+    const cloud3 = new THREE.Mesh(
+        new THREE.SphereGeometry(0.5),
+        new THREE.MeshBasicMaterial({ color: colors.cloud }),
+    )
+    const cloud4 = new THREE.Mesh(
+        new THREE.SphereGeometry(0.3),
+        new THREE.MeshBasicMaterial({ color: colors.cloud }),
+    )
+    cloud1.position.x = -1.4;
+    cloud1.position.y = 0.1;
+    cloud2.position.x = -0.9;
+    cloud3.position.x = -0.3;
+    cloud4.position.x = 0.1;
+    cloudGroup.add(cloud1, cloud2, cloud3, cloud4);
+    cloudGroup.position.z = - groundH / 2 - 1;
+    cloudGroup.position.y = 2.5;
+
+    cloudGroup1 = cloudGroup.clone();
+
+    cloudGroup1.position.x = 4;
+    cloudGroup1.position.y = 2.5;
+    cloudGroup1.scale.set(0.5, 0.5, 0.5);
+    roadGroup.add(plane, leftLine, rightLine, dashGroup)
+    groundGroup.add(roadGroup, frontGrass, backGrass);
+    groundGroup.rotation.x = -0.5 * Math.PI;
+    scene.add(groundGroup, treeGroup, buildGroup, cloudGroup, cloudGroup1);
 }
 
-// 白云
-const cloudGroup = new THREE.Group();
-const cloud1 = new THREE.Mesh(
-    new THREE.SphereGeometry(0.4),
-    new THREE.MeshBasicMaterial({ color: colors.cloud }),
-)
-const cloud2 = new THREE.Mesh(
-    new THREE.SphereGeometry(0.6),
-    new THREE.MeshBasicMaterial({ color: colors.cloud }),
-)
-const cloud3 = new THREE.Mesh(
-    new THREE.SphereGeometry(0.5),
-    new THREE.MeshBasicMaterial({ color: colors.cloud }),
-)
-const cloud4 = new THREE.Mesh(
-    new THREE.SphereGeometry(0.3),
-    new THREE.MeshBasicMaterial({ color: colors.cloud }),
-)
-cloud1.position.x = -1.4;
-cloud1.position.y = 0.1;
-cloud2.position.x = -0.9;
-cloud3.position.x = -0.3;
-cloud4.position.x = 0.1;
-cloudGroup.add(cloud1, cloud2, cloud3, cloud4);
-cloudGroup.position.z = - groundH / 2 - 1;
-cloudGroup.position.y = 2.5;
-
-const cloudGroup1 = cloudGroup.clone();
-
-cloudGroup1.position.x = 4;
-cloudGroup1.position.y = 2.5;
-cloudGroup1.scale.set(0.5, 0.5, 0.5);
-roadGroup.add(plane, leftLine, rightLine, dashGroup)
-groundGroup.add(roadGroup, frontGrass, backGrass);
-groundGroup.rotation.x = -0.5 * Math.PI;
-scene.add(groundGroup, treeGroup, buildGroup, cloudGroup, cloudGroup1);
-
-
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize(w, h);
-renderer.setClearColor(colors.background);
-document.body.appendChild(renderer.domElement);
-
-const controls = new OrbitControls(camera, renderer.domElement);
-const clock = new THREE.Clock();
-animate();
 function animate() {
     const time = clock.getElapsedTime();
     requestAnimationFrame(animate);
@@ -215,4 +237,3 @@ function animate() {
     renderer.render(scene, camera);
     controls.update();
 };
-
